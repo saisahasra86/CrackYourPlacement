cc855fe69872dd41571eb90337f32f2243c411bb
Surkanti Sai Sahasra <107041256+saisahasra86@users.noreply.github.com>
2024-08-22 22:42:40 +0530

    Add files via upload

---
+++ BestTimetoBuyAndSellStock2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std
+
+ int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            if(prices[i]<prices[i+1]){
+                ans+=(prices[i+1]-prices[i]);
+            }
+        }
+        return ans;
+    }
