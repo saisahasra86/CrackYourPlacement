c9dcb85051dd9e2f8e2d521cac2d41c98c90a673
Surkanti Sai Sahasra <107041256+saisahasra86@users.noreply.github.com>
2024-08-25 22:57:57 +0530

    Add files via upload

---
+++ PermutationsInAnArray.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std
+
+ bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        // Your code goes here
+        sort(arr1.begin(),arr1.end());
+        sort(arr2.begin(),arr2.end(),greater<int>());
+        int n=arr1.size();
+        for(int i=0;i<n;i++){
+            if((arr1[i]+arr2[i])<k){
+                return false;
+            }
+        }
+        return true;
+        
+    }
